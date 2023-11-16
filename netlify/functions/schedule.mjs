c192df6d0975aec4e71051a4dc2c34835e65a0e2
cmfwmedia <tim@cmfwmedia.com>
2023-11-16 15:32:20 -0600

    fix json function, made variable

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import dotenv  from "dotenv"
+import dotenv from "dotenv"
 import { createClient } from '@supabase/supabase-js'
 
 dotenv.config()
@@ -9,46 +9,40 @@ const supabaseUrl = process.env.DATABASE_URL
 const supabaseKey = process.env.SUPABASE_SERVICE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+const testJSON = {
+    elements: [
+        {
+            id: 1,
+            name: 'Element 1',
+            value: 10,
+            description: 'This is element 1'
+        },
+        {
+            id: 2,
+            name: 'Element 2',
+            value: 20,
+            description: 'This is element 2'
+        },
+        {
+            id: 3,
+            name: 'Element 3',
+            value: 30,
+            description: 'This is element 3'
+        }
+        // Add more elements as needed
+    ]
+};
+
 export default async (req) => {
     const { next_run } = await req.json()
 
-    function createTestJSON() {
-        const testJSON = {
-            elements: [
-                {
-                    id: 1,
-                    name: 'Element 1',
-                    value: 10,
-                    description: 'This is element 1'
-                },
-                {
-                    id: 2,
-                    name: 'Element 2',
-                    value: 20,
-                    description: 'This is element 2'
-                },
-                {
-                    id: 3,
-                    name: 'Element 3',
-                    value: 30,
-                    description: 'This is element 3'
-                }
-                // Add more elements as needed
-            ]
-        };
-
-        return testJSON;
-    }
-
-    // Example usage:
-    const testJSONData = createTestJSON();
 
     try {
         const { data, error } = await supabase
             .from('jsontest') // Replace 'your_table_name' with the actual table name
             .insert([
                 {
-                    jsoncol: testJSONData, // Replace column1, column2, ... with your column names
+                    jsoncol: testJSON, // Replace column1, column2, ... with your column names
                     // Add more columns and values as needed
                 },
             ]);
@@ -63,7 +57,7 @@ export default async (req) => {
         console.error('Error adding row:', error.message);
     }
     console.log("Received event! Next invocation at:", next_run)
-    console.log(testJSONData);
+    console.log(testJSON);
 
 }
 
