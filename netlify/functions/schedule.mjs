0a98c3e233b14475ed931591681839c84adde702
cmfwmedia <tim@cmfwmedia.com>
2023-11-16 16:15:51 -0600

    new attempt at reading a column

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -34,12 +34,34 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async (req) => {
     // Get everything from the notes table
-    let { data: notes, error } = await supabase
-        .from('jasontest')
-        .select('testText')
+    // let { data: notes, error } = await supabase
+    //     .from('jasontest')
+    //     .select('testText')
 
-        console.log(notes)
-        console.log(error)
+    //     console.log(notes)
+    //     console.log(error)
+
+    try {
+        const { data, error } = await supabase
+            .from('jasontest')
+            .select('testText')
+            .order('createdAt', { ascending: false })
+            .limit(1);
+
+        if (error) {
+            console.error('Error fetching data:', error.message);
+            return;
+        }
+
+        if (data.length > 0) {
+            const textTestValue = data[0].textTest;
+            console.log('Value of textTest column from the most recent row:', textTestValue);
+        } else {
+            console.log('No rows found in the table');
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+    }
 
 }
 
