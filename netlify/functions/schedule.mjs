2588ac917a3620dd28b2c2ed8685cebb5b78b0a7
cmfwmedia <tim@cmfwmedia.com>
2023-11-16 15:47:30 -0600

    test

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -1,45 +1,47 @@
 import fetch from 'node-fetch'
 import dotenv from "dotenv"
 import { createClient } from '@supabase/supabase-js'
-
 dotenv.config()
-
-// Initialize Supabase client
 const supabaseUrl = process.env.DATABASE_URL
 const supabaseKey = process.env.SUPABASE_SERVICE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const simpleJSON = {
-    name: 'John Doe',
-    age: 30,
-    email: 'john@example.com'
-};
 
-export default async (req) => {
-    const { next_run } = await req.json()
+// export default async (req) => {
+//     const { next_run } = await req.json()
+//     try {
+//         const { data, error } = await supabase
+//             .from('jsontest') // Replace 'your_table_name' with the actual table name
+//             .insert([
+//                 {
+//                     testText: "this is a text test", // Replace column1, column2, ... with your column names
+//                     // Add more columns and values as needed
+//                 },
+//             ]);
 
+//         if (error) {
+//             console.error('Error adding row:', error.message);
+//             return;
+//         }
 
-    try {
-        const { data, error } = await supabase
-            .from('jsontest') // Replace 'your_table_name' with the actual table name
-            .insert([
-                {
-                    testText: "this is a text test", // Replace column1, column2, ... with your column names
-                    // Add more columns and values as needed
-                },
-            ]);
+//         console.log('Row added successfully:', data);
+//         console.log("Received event! Next invocation at:", next_run)
 
-        if (error) {
-            console.error('Error adding row:', error.message);
-            return;
-        }
+//     } catch (error) {
+//         console.error('Error adding row:', error.message);
+//     }
+// }
 
-        console.log('Row added successfully:', data);
-        console.log("Received event! Next invocation at:", next_run)
+export const handler = async (event) => {
+    // Insert a row
+    const { data, error } = await supabase
+        .from('jsontest')
+        .insert([
+            { testText: 'I need to not forget this' },
+        ]);
 
-    } catch (error) {
-        console.error('Error adding row:', error.message);
-    }
+    // Did it work?
+    console.log(data, error);
 }
 
 export const config = {
