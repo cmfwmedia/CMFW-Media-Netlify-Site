b0200dc30d01e11f6fa6dc6470cbb5a56a0f0ff0
cmfwmedia <tim@cmfwmedia.com>
2023-11-16 18:06:38 -0600

    make api call in schedule function, console log response

--- netlify/functions/schedule.mjs
+++ netlify/functions/schedule.mjs
@@ -6,62 +6,19 @@ const supabaseUrl = process.env.DATABASE_URL
 const supabaseKey = process.env.SUPABASE_SERVICE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
-// export default async (req) => {
-//     const { next_run } = await req.json()
-//     try {
-//         const { data, error } = await supabase
-//             .from('jsontest') // Replace 'your_table_name' with the actual table name
-//             .insert([
-//                 {
-//                     testText: "this is a text test", // Replace column1, column2, ... with your column names
-//                     // Add more columns and values as needed
-//                 },
-//             ]);
-
-//         if (error) {
-//             console.error('Error adding row:', error.message);
-//             return;
-//         }
-
-//         console.log('Row added successfully:', data);
-//         console.log("Received event! Next invocation at:", next_run)
-
-//     } catch (error) {
-//         console.error('Error adding row:', error.message);
-//     }
-// }
-
 export default async (req) => {
-    // Get everything from the notes table
-    // let { data: notes, error } = await supabase
-    //     .from('jasontest')
-    //     .select('testText')
 
-    //     console.log(notes)
-    //     console.log(error)
+    const apiKey = process.env.DRONELOGBOOK_API_KEY
+    const url = `https://api.dronelogbook.com/drone`;
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'apiKey': `${apiKey}`,
+        }
+    });
 
-    // try {
-    //     const { data, error } = await supabase
-    //         .from('jsonTest')
-    //         .select('example');
-
-    //     if (error) {
-    //         console.error('Error fetching data:', error.message);
-    //         return;
-    //     }
-
-    //     if (data.length > 0) {
-    //         console.log('Values under the "example" column:');
-    //         data.forEach(row => {
-    //             console.log(row.example);
-    //         });
-    //     } else {
-    //         console.log('No rows found in the table');
-    //     }
-    // } catch (error) {
-    //     console.error('Error fetching data:', error.message);
-    // }
+    console.log(response)
 
     try {
         const { data, error } = await supabase
@@ -78,8 +35,6 @@ export default async (req) => {
         console.error('Error adding row:', error.message);
     }
 }
-
-
 
 export const config = {
     schedule: "* * * * *"
