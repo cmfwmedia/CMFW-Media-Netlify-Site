7dbd1bca4e8e503819e88d6e7caea4b2986529f0
cmfwmedia <tim@cmfwmedia.com>
2023-11-26 14:47:17 -0600

    make schedule return code 200

--- netlify/functions/scheduleTest.js
+++ netlify/functions/scheduleTest.js
@@ -2,6 +2,9 @@ export default async (req) => {
     const { next_run } = await req.json()
 
     console.log("Received event! Next invocation at:", next_run)
+    return {
+        statusCode: 200,
+    };
 }
 
 export const config = {
