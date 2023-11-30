fb1223486ccb4230f95dd20844c31d6c202e99ec
cmfwmedia <tim@cmfwmedia.com>
2023-11-30 13:18:58 -0600

    copy netlify functions from main

---
+++ netlify/functions/pushCustomers.js
@@ -0,0 +1,13 @@
+export default async (req) => {
+    const { next_run } = await req.json()
+
+    console.log("Received event! Next invocation at:", next_run)
+
+    const response = await fetch('https://cmfw-media-netlify-site.netlify.app/.netlify/functions/getCustomers')
+        .then(response => response.json()
+        )
+}
+
+export const config = {
+    schedule: "0 */12 * * *"
+}
---
+++ netlify/functions/test.js
@@ -0,0 +1,37 @@
+import fetch from 'node-fetch'
+require('dotenv').config()
+import { createClient } from '@supabase/supabase-js'
+const apiUrl = 'https://api.dronelogbook.com/company';
+const apiKey = process.env.DRONELOGBOOK_API_KEY;
+const supabaseUrl = process.env.DATABASE_URL
+const supabaseKey = process.env.SUPABASE_SERVICE_API_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+export const handler = async (event, context) => {
+    try {
+        const testdata = "lolol";
+
+        // Push flightData to Supabase
+        const { data, error } = await supabase
+            .from('dronelogbook api container')
+            .update([{ 'test': testdata }])
+            .match({ id: 1 });
+
+        if (error) {
+            throw new Error(error.message);
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'company data pushed to Supabase' }),
+        };
+    } catch (error) {
+        console.error('Error:', error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to push data to Supabase' }),
+        };
+    }
+}
